3db74216be0f57c8a12ab73c1e8f3d0af49f64e1
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2025-04-23 13:12:40 +0200

    Updates

--- obj/Debug/net8.0/SoftyComp.SAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoftyComp.SAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftyComp.SAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434496c02244e2d43f7fbacb855fb7873127fcf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftyComp.SAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftyComp.SAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
